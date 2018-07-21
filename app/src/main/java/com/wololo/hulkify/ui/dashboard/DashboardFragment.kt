0d5f658f0f9d02f938dd6ffe0dc07e5918e8eea9
Mehmet Ayan <mehmet.ayan@generalmobile.com>
2018-07-21 11:33:58 +0300

    base fragment fixed

--- app/src/main/java/com/wololo/hulkify/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/wololo/hulkify/ui/dashboard/DashboardFragment.kt
@@ -1,34 +1,19 @@
 package com.wololo.hulkify.ui.dashboard
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.wololo.hulkify.R
 import com.wololo.hulkify.core.BaseAdapter
 import com.wololo.hulkify.core.BaseFragment
 import com.wololo.hulkify.databinding.ActivityDashboardBinding
 import com.wololo.hulkify.pojo.CalendarEntity
 
-class DashboardFragment : BaseFragment<DashboardViewModel, ActivityDashboardBinding>() {
+class DashboardFragment : BaseFragment<DashboardViewModel, ActivityDashboardBinding>(DashboardViewModel::class.java) {
     override fun getLayoutRes(): Int {
         return R.layout.activity_dashboard
     }
 
-    override fun getViewModelKey(): Class<DashboardViewModel> {
-        return DashboardViewModel::class.java
-    }
+    override fun init() {
+        super.init()
 
-    override fun initViewModel(viewModel: DashboardViewModel) {
-        binding.viewModel = viewModel
-    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        init()
-        return super.onCreateView(inflater, container, savedInstanceState)
-    }
-
-    private fun init() {
         binding.calendarRecycler.adapter = DashboardAdapter()
         (binding.calendarRecycler.adapter as BaseAdapter<CalendarEntity>).submitList(viewModel.getCalendar())
     }
