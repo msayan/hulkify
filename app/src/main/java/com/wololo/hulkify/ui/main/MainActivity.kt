1afc853ccd76587c6207ca3615b6fc5ef608eb6e
Mehmet Ayan <mehmet.ayan@generalmobile.com>
2018-07-21 14:24:14 +0300

    Main Activity now fullscreen

--- app/src/main/java/com/wololo/hulkify/ui/main/MainActivity.kt
+++ app/src/main/java/com/wololo/hulkify/ui/main/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.support.design.widget.CoordinatorLayout
+import android.view.View
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.ImageView
 import com.wololo.hulkify.R
@@ -15,6 +16,14 @@ import com.wololo.hulkify.ui.loki.LokiActivity
 import com.wololo.hulkify.ui.music.MusicActivity
 import com.wololo.hulkify.utils.extensions.hasOreo
 import com.wololo.hulkify.utils.service.SensorService
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+import android.view.View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+import android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+import android.view.View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+import android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+import android.view.View.SYSTEM_UI_FLAG_IMMERSIVE
+import android.view.WindowManager
+
 
 class MainActivity : BaseActivity<MainActivityViewModel, ActivityMainBinding>() {
 
@@ -25,6 +34,8 @@ class MainActivity : BaseActivity<MainActivityViewModel, ActivityMainBinding>() 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+
         startSensorService()
         binding.rootView.setOnTouchListener { v, event ->
             printCrash(event.x, event.y);true
@@ -34,6 +45,7 @@ class MainActivity : BaseActivity<MainActivityViewModel, ActivityMainBinding>() 
     override fun initViewModel(viewModel: MainActivityViewModel) {
         binding.viewModel = viewModel
     }
+
 
     fun printCrash(x: Float, y: Float) {
         val imageView = ImageView(this)
